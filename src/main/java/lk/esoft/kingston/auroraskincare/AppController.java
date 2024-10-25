cc55a1f806fb261a1fdf820483832faa07a3162f
Menith <menith.somarathne@gmail.com>
2024-10-26 01:11:54 +0530

    finalize

--- src/main/java/lk/esoft/kingston/auroraskincare/AppController.java
+++ src/main/java/lk/esoft/kingston/auroraskincare/AppController.java
@@ -3,7 +3,7 @@ package lk.esoft.kingston.auroraskincare;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class AppController {
     @FXML
     private Label welcomeText;
 
